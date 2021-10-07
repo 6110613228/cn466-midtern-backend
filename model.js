58cd10ecb9cb3c19e71edd546daa36ea73d01853
Poonnatuch Boonyarattanasoontorn <6110613228@student.tu.ac.th>
2021-10-07 18:13:29 +0700

    back

--- model.js
+++ model.js
@@ -36,12 +36,16 @@ mqttClient.on("connect", () => {
   });
 });
 
+var acceleration = [];
 mqttClient.on("message", async (topic, payload) => {
   // Data from payload
   let data = JSON.parse(payload);
-  delete data["acceleration"];
-  delete data["angular_velocity"];
 
+  console.log(acceleration);
+
+  delete data["angular_velocity"];
+  delete data["acceleration"];
+  
   data["timestamp"] = new Date().toLocaleString("th", { timeZone: 'Asia/Bangkok' });
   data["IncubatorID"] = Math.round(Math.random() * (3 - 1) + 1);
 
